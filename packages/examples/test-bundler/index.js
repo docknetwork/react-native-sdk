0b511680db957c3cbeb0c9290ef51ff74a1a1c4e
Maycon Mello <maycon.mellos@gmail.com>
2022-01-24 19:05:19 -0300

    refactor

--- packages/examples/test-bundler/index.js
+++ packages/examples/test-bundler/index.js
@@ -1,10 +1,5 @@
 const {build} = require('@docknetwork/wallet-sdk-bundler/webpack');
 
-// build({
-//   input: require.resolve('./bundler-test.js'),
-//   outputDir: './bundler-test',
-// });
-
 build({
   entry: require.resolve('./bundler-test.js'),
   path: `${__dirname}/build`,
