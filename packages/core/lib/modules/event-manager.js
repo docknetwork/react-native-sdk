ec4ce3d0b968d0de1afee5bac5e6e23a48edb319
Maycon Mello <maycon.mellos@gmail.com>
2022-01-25 15:08:44 -0300

    refactor

--- packages/core/lib/modules/event-manager.js
+++ packages/core/lib/modules/event-manager.js
@@ -1,4 +1,5 @@
 import {EventEmitter, once} from 'events';
+import assert from 'assert';
 
 export class EventManager {
   eventEmitter: EventEmitter;
@@ -23,36 +24,44 @@ export class EventManager {
     );
   }
 
-  getEventByName(eventName) {
+  getEventByName(eventName: string) {
+    assert(!!eventName, 'eventName is required');
+
     return this.eventNames.find(e => e === eventName);
   }
 
-  checkEvent(eventName) {
-    if (!this.getEventByName(eventName)) {
-      throw new Error(`Event with name "${eventName}" not found`);
-    }
+  assertEvent(eventName) {
+    assert(
+      this.getEventByName(eventName),
+      `Event with name "${eventName}" not found`,
+    );
   }
 
-  emit(eventName, payload) {
-    this.checkEvent(eventName);
+  emit(eventName: string, payload?: any) {
+    this.assertEvent(eventName);
     this.eventEmitter.emit(eventName, payload);
     return this;
   }
 
   removeListener(eventName, callback) {
-    this.checkEvent(eventName);
+    this.assertEvent(eventName);
     this.eventEmitter.removeListener(eventName, callback);
     return this;
   }
 
-  on(eventName, payload) {
-    this.checkEvent(eventName);
-    this.eventEmitter.on(eventName, payload);
+  addEventListener(eventName, callback) {
+    return this.on(eventName, callback);
+  }
+
+  on(eventName, callback) {
+    assert(!!callback, 'callback is required');
+    this.assertEvent(eventName);
+    this.eventEmitter.on(eventName, callback);
     return this;
   }
 
   waitFor(eventName) {
-    this.checkEvent(eventName);
+    this.assertEvent(eventName);
     return once(this.eventEmitter, eventName);
   }
 }
