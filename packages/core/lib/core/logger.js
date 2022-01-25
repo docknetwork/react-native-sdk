ebf3292594abcee0aa18d801c7f6e0ba93a898cc
Maycon Mello <maycon.mellos@gmail.com>
2022-01-25 15:00:58 -0300

    refactor

--- packages/core/lib/core/logger.js
+++
@@ -1,8 +0,0 @@
-import {LOGGER_LEVEL} from '@env';
-const winston = require('winston');
-
-export const Logger = winston.createLogger({
-  format: winston.format.simple(),
-  level: LOGGER_LEVEL || 'info',
-  transports: [new winston.transports.Console()],
-});
