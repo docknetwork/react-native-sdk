525b51a9578af0a01b7f5ce0fb68cd8146782c82
Maycon Mello <maycon.mellos@gmail.com>
2022-01-25 10:50:24 -0300

    refactor

--- packages/transactions/lib/hooks.js
+++ packages/transactions/lib/hooks.js
@@ -1,48 +1,47 @@
-/* eslint-disable */
 // Work in progress
 // Examples on how to integrate with react
 // Will be moved to a separate package
 
-import {useEffect, useState} from 'react';
-import {TransactionEvents, Transactions} from './transactions';
+// import {useEffect, useState} from 'react';
+// import {TransactionEvents, Transactions} from './transactions';
 
-function useTransactions() {
-  const [transactions, setTransactions] = useState();
+// function useTransactions() {
+//   const [transactions, setTransactions] = useState();
 
-  useEffect(() => {
-    const txModule = Transactions.getInstance();
+//   useEffect(() => {
+//     const txModule = Transactions.getInstance();
 
-    const handleChange = () => {
-      txModule.getAll().then(setTransactions);
-    };
+//     const handleChange = () => {
+//       txModule.getAll().then(setTransactions);
+//     };
 
-    txModule.eventManager
-      .on(TransactionEvents.added)
-      .on(TransactionEvents.updated);
-  }, []);
-}
+//     txModule.eventManager
+//       .on(TransactionEvents.added)
+//       .on(TransactionEvents.updated);
+//   }, []);
+// }
 
-function useTransactionBuilder(address) {}
+// function useTransactionBuilder(address) {}
 
-function Test() {
-  const {
-    transactions,
-    handleSend,
-    setInput,
-    loading,
-    handleChange,
-    fee,
-    isValid,
-    errors,
-    // refreshFee,
-    retry,
-  } = useTransactionBuilder('test-address');
+// function Test() {
+//   const {
+//     transactions,
+//     handleSend,
+//     setInput,
+//     loading,
+//     handleChange,
+//     fee,
+//     isValid,
+//     errors,
+//     // refreshFee,
+//     retry,
+//   } = useTransactionBuilder('test-address');
 
-  return (
-    <div>
-      <input onChange={handleChange('amount')} />
-      <input onChange={handleChange('toAddress')} />
-      <button onChange={handleSend} />
-    </div>
-  );
-}
+//   return (
+//     <div>
+//       <input onChange={handleChange('amount')} />
+//       <input onChange={handleChange('toAddress')} />
+//       <button onChange={handleSend} />
+//     </div>
+//   );
+// }
