8077b073551c2edfa15e0fe174695430e76bedd4
Maycon Mello <maycon.mellos@gmail.com>
2022-01-25 17:00:55 -0300

    refactor

--- packages/core/lib/services/logger.js
+++ packages/core/lib/services/logger.js
@@ -2,9 +2,8 @@ export default {
   name: 'logger',
   routes: {
     async log(...params) {
+      // TODO: start using winston
       console.log(...params);
-
-      return 'ok';
     },
   },
 };
