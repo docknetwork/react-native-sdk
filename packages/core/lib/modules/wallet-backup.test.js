0260e9c89589bf3e2ab461871840e90b503d861e
Maycon Mello <maycon.mellos@gmail.com>
2022-02-09 12:40:24 -0300

    refactor

--- packages/core/lib/modules/wallet-backup.test.js
+++ packages/core/lib/modules/wallet-backup.test.js
@@ -1,6 +1,6 @@
 import {getPromiseError} from '../services/test-utils';
 import {walletService} from '../services/wallet/service';
-import backup11 from '../test/fixtures/backup-files/wallet-backup-11.json';
+import emptyBackup from '../test/fixtures/backup-files/empty-wallet-backup.json';
 import {WalletBackup} from './wallet-backup';
 
 describe('WalletBackup', () => {
@@ -16,7 +16,7 @@ describe('WalletBackup', () => {
 
     it('expect to handle empty file', async () => {
       const result = await getPromiseError(() =>
-        WalletBackup.getInstance().validate(backup11, 'test'),
+        WalletBackup.getInstance().validate(emptyBackup, 'test'),
       );
       expect(result.message).toBeDefined();
     });
