9c57231262442fd55c4c5d080544d6fe66ac1e42
Ifeanyi Osinakayah <ifeanyiosinakayah15@gmail.com>
2022-05-12 23:06:58 +0100

    Remove logs

--- packages/dids/lib/index.js
+++ packages/dids/lib/index.js
@@ -1,6 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.DidKeyManager = void 0;
 const did_key_manager_1 = require("./did.methods/did.key.manager");
-did_key_manager_1.DidKeyManager.getInstance().saveDID('did:key:z6MkwdAManAKt5L5tp1BChkzfH1vESsdUbpnBcsnedsqqPRV').then((res) => {
-    console.log(res);
-});
+Object.defineProperty(exports, "DidKeyManager", { enumerable: true, get: function () { return did_key_manager_1.DidKeyManager; } });
