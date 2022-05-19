b69ce8ea06a663d3ffb3a881c549b5d56d49e41a
Ifeanyi Osinakayah <ifeanyiosinakayah15@gmail.com>
2022-05-19 18:26:39 +0100

    Lint

--- packages/dids/lib/index.js
+++ packages/dids/lib/index.js
@@ -1,46 +1,44 @@
-"use strict";
+'use strict';
 
-Object.defineProperty(exports, "__esModule", {
-  value: true
+Object.defineProperty(exports, '__esModule', {
+  value: true,
 });
 exports.DIDKeyManager = void 0;
-
-var _uuid = require("uuid");
+var _uuid = require('uuid');
 
 const didKeyDriver = require('@digitalbazaar/did-method-key').driver();
 
-const DID_DEFAULT_CONTEXT = ["https://w3id.org/wallet/v1", "https://w3id.org/did-resolution/v1"];
+const DID_DEFAULT_CONTEXT = [
+  'https://w3id.org/wallet/v1',
+  'https://w3id.org/did-resolution/v1',
+];
 
-const DIDKeyManager = function () {
+const DIDKeyManager = (function () {
   const createDID = async keyDoc => {
-    const {
-      didDocument
-    } = await didKeyDriver._keyPairToDidDocument({
-      keyPair: { ...keyDoc,
-        keyPair: keyDoc
-      }
+    const {didDocument} = await didKeyDriver._keyPairToDidDocument({
+      keyPair: {...keyDoc, keyPair: keyDoc},
     }); // add context
 
     const expiryDate = new Date();
     expiryDate.setFullYear(expiryDate.getFullYear() + 100);
     const didResolution = {
-      "@context": DID_DEFAULT_CONTEXT,
+      '@context': DID_DEFAULT_CONTEXT,
       id: (0, _uuid.v4)(),
-      type: ["DIDResolutionResponse"],
+      type: ['DIDResolutionResponse'],
       correlation: [],
       created: new Date().toISOString(),
       expires: expiryDate.toISOString(),
-      didDocument
+      didDocument,
     };
     return {
       didResolution,
-      keyDoc
+      keyDoc,
     };
   };
 
   return {
-    createDID
+    createDID,
   };
-}();
+})();
 
 exports.DIDKeyManager = DIDKeyManager;
