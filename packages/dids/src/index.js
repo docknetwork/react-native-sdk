dcaaf5a939bf36b1fc67b8c48a18e5220fe75995
Ifeanyi Osinakayah <ifeanyiosinakayah15@gmail.com>
2022-05-19 22:54:47 +0100

    Removed unsued variables

--- packages/dids/src/index.js
+++ packages/dids/src/index.js
@@ -1,12 +1,7 @@
-import {v4 as uuidv4} from 'uuid';
 import {driver} from '@digitalbazaar/did-method-key';
 
 const didKeyDriver = driver();
 
-const DID_DEFAULT_CONTEXT = [
-  'https://w3id.org/wallet/v1',
-  'https://w3id.org/did-resolution/v1',
-];
 export const DIDKeyManager = (function () {
   const keypairToDidKeyDocument = async keyDoc => {
     const {didDocument} = await didKeyDriver._keyPairToDidDocument({
