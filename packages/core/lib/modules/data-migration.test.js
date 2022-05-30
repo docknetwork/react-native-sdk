d863ff8e5b0e682d9c2451c9192da12f639e9627
Ifeanyi Osinakayah <ifeanyiosinakayah15@gmail.com>
2022-05-30 20:25:00 +0100

    Lint

--- packages/core/lib/modules/data-migration.test.js
+++ packages/core/lib/modules/data-migration.test.js
@@ -1,13 +1,10 @@
 import {Wallet} from './wallet';
-import {mockDockService} from '../services/test-utils';
 import walletLegacyData from '../test/fixtures/lagacy-wallet-schema.json';
 
 describe('DataMigration', () => {
-  let unmockDockService;
   let wallet: Wallet;
 
   beforeAll(async () => {
-    unmockDockService = await mockDockService();
     global.localStorage.setItem(
       'dock-wallet',
       JSON.stringify(walletLegacyData),
