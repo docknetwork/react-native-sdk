619094bb33e09063233aade1e2de25e57628dc48
Ifeanyi Osinakayah <ifeanyiosinakayah15@gmail.com>
2022-06-01 13:03:14 +0100

    Signing credentials

---
+++ packages/core/lib/services/credential/service.js
@@ -0,0 +1,34 @@
+import {serviceName, validation} from './config';
+import VerifiableCredential from '@docknetwork/sdk/verifiable-credential';
+import {getKeypairFromDoc} from '@docknetwork/wallet/methods/keypairs';
+import {getSuiteFromKeyDoc} from '@docknetwork/sdk/utils/vc/helpers';
+
+class CredentialService {
+  constructor() {
+    this.name = serviceName;
+  }
+  rpcMethods = [
+    CredentialService.prototype.generateCredential,
+    CredentialService.prototype.signCredential,
+  ];
+  generateCredential() {
+    return new VerifiableCredential();
+  }
+  async signCredential(params) {
+    validation.signCredential(params);
+    const {vcJson, keyDoc} = params;
+    const verifiableCredential = new VerifiableCredential();
+    verifiableCredential.setFromJSON(vcJson);
+    const kp = getKeypairFromDoc(keyDoc);
+
+    kp.signer = kp.signer();
+    const suite = getSuiteFromKeyDoc(kp);
+    verifiableCredential.setIssuer(keyDoc.controller);
+
+    await verifiableCredential.sign(suite);
+
+    return verifiableCredential;
+  }
+}
+
+export const credentialService = new CredentialService();
