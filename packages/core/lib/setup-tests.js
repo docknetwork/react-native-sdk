3d9a2ba24856a54bc7c2bb2d81ac523efacda796
Maycon Mello <maycon.mellos@gmail.com>
2022-06-06 12:21:43 -0300

    fixing issues with wallet migration

--- packages/core/lib/setup-tests.js
+++ packages/core/lib/setup-tests.js
@@ -1,5 +1,8 @@
 import {getRpcClient, initRpcClient} from './rpc-client';
 import rpcServer from './rpc-server';
+import {setStorage} from './core/storage';
+
+setStorage(global.localStorage);
 
 initRpcClient(req => {
   return rpcServer.receive(req).then(result => {
