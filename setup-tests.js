a833e64d34fa60435eb8f344dec268fbbf561367
Ifeanyi Osinakayah <ifeanyiosinakayah15@gmail.com>
2022-07-19 15:16:34 +0100

    Unit test

--- setup-tests.js
+++ setup-tests.js
@@ -44,3 +44,74 @@ jest.mock('@docknetwork/wallet-sdk-dids', () => {
     DIDKeyManager: moduleFunctions,
   };
 });
+
+jest.mock('@docknetwork/wallet-sdk-core/lib/services/dids', () => {
+  const originalModule = jest.requireActual(
+    '@docknetwork/wallet-sdk-core/lib/services/dids',
+  );
+  const didDocument = {
+    '@context': [
+      'https://www.w3.org/ns/did/v1',
+      'https://ns.did.ai/transmute/v1',
+      {
+        '@base': 'did:key:z6Mks8mvCnVx4HQcoq7ZwvpTbMnoRGudHSiEpXhMf6VW8XMg',
+      },
+    ],
+    id: 'did:key:z6Mks8mvCnVx4HQcoq7ZwvpTbMnoRGudHSiEpXhMf6VW8XMg',
+    verificationMethod: [
+      {
+        id: '#z6Mks8mvCnVx4HQcoq7ZwvpTbMnoRGudHSiEpXhMf6VW8XMg',
+        type: 'JsonWebKey2020',
+        controller: 'did:key:z6Mks8mvCnVx4HQcoq7ZwvpTbMnoRGudHSiEpXhMf6VW8XMg',
+        publicKeyJwk: {
+          crv: 'Ed25519',
+          x: 'vGur-MEOrN6GDLf4TBGHDYAERxkmWOjTbztvG3xP0I8',
+          kty: 'OKP',
+        },
+      },
+      {
+        id: '#z6LScrLMVd9jvbphPeQkGffSeB99EWSYqAnMg8rGiHCgz5ha',
+        type: 'JsonWebKey2020',
+        controller: 'did:key:z6Mks8mvCnVx4HQcoq7ZwvpTbMnoRGudHSiEpXhMf6VW8XMg',
+        publicKeyJwk: {
+          kty: 'OKP',
+          crv: 'X25519',
+          x: 'EXXinkMxdA4zGmwpOOpbCXt6Ts6CwyXyEKI3jfHkS3k',
+        },
+      },
+    ],
+    authentication: ['#z6Mks8mvCnVx4HQcoq7ZwvpTbMnoRGudHSiEpXhMf6VW8XMg'],
+    assertionMethod: ['#z6Mks8mvCnVx4HQcoq7ZwvpTbMnoRGudHSiEpXhMf6VW8XMg'],
+    capabilityInvocation: ['#z6Mks8mvCnVx4HQcoq7ZwvpTbMnoRGudHSiEpXhMf6VW8XMg'],
+    capabilityDelegation: ['#z6Mks8mvCnVx4HQcoq7ZwvpTbMnoRGudHSiEpXhMf6VW8XMg'],
+    keyAgreement: ['#z6LScrLMVd9jvbphPeQkGffSeB99EWSYqAnMg8rGiHCgz5ha'],
+  };
+  const moduleFunctions = {
+    getDIDResolution: jest.fn().mockReturnValue({
+      id: new Date().getTime().toString(),
+      type: 'DIDResolutionResponse',
+      didDocument,
+      correlation: [],
+    }),
+    generateKeyDoc: jest.fn().mockReturnValue({
+      '@context': ['https://w3id.org/wallet/v1'],
+      id: 'urn:uuid:e8fc7810-9524-11ea-bb37-0242ac130002',
+      name: 'My Test Key 2',
+      image: 'https://via.placeholder.com/150',
+      description: 'For testing only, totally compromised.',
+      tags: ['professional', 'organization', 'compromised'],
+      correlation: [],
+      controller: 'did:key:z6MkjjCpsoQrwnEmqHzLdxWowXk5gjbwor4urC1RPDmGeV8r',
+      type: 'Ed25519VerificationKey2018',
+      privateKeyBase58:
+        '3CQCBKF3Mf1tU5q1FLpHpbxYrNYxLiZk4adDtfyPEfc39Wk6gsTb2qoc1ZtpqzJYdM1rG4gpaD3ZVKdkiDrkLF1p',
+      publicKeyBase58: '6GwnHZARcEkJio9dxPYy6SC5sAL6PxpZAB6VYwoFjGMU',
+    }),
+    keypairToDIDKeyDocument: jest.fn().mockReturnValue(didDocument),
+  };
+
+  return {
+    ...originalModule,
+    didServiceRPC: moduleFunctions,
+  };
+});
