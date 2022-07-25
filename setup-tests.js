485c4bf66e65f1efabedfec2a9bfe255388f6c9b
Maycon Mello <maycon.mellos@gmail.com>
2022-07-25 16:53:35 -0300

    fixing issue with tests

--- setup-tests.js
+++ setup-tests.js
@@ -4,9 +4,12 @@ import {NetworkManager} from './packages/core/lib/modules/network-manager';
 import {getStorage} from './packages/core/lib/core/storage';
 import './packages/transactions/lib/schema';
 import {initRealm} from '@docknetwork/wallet-sdk-core/lib/core/realm';
+import {mockDockService} from '@docknetwork/wallet-sdk-core/lib/services/test-utils';
 
 initRealm();
 NetworkManager.getInstance().setNetworkId('testnet');
+
+mockDockService();
 
 process.env.ENCRYPTION_KEY =
   '776fe87eec8c9ba8417beda00b23cf22f5e134d9644d0a195cd9e0b7373760c1';
