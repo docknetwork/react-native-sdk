f6790131943841b00f05d17e22685dd626c7f671
Ifeanyi Osinakayah <ifeanyiosinakayah15@gmail.com>
2022-08-29 15:09:48 +0100

    Presentation hooks

--- packages/react-native/lib/credentialPresentHooks.js
+++ packages/react-native/lib/credentialPresentHooks.js
@@ -2,12 +2,17 @@ import {useCallback, useMemo} from 'react';
 import {credentialServiceRPC} from '@docknetwork/wallet-sdk-core/lib/services/credential';
 
 export function usePresentation() {
-  const presentCredentials = useCallback(async (credentials, keyDoc) => {
-    return credentialServiceRPC.createPresentation({
-      credentials,
-      keyDoc,
-    });
-  }, []);
+  const presentCredentials = useCallback(
+    async ({credentials, keyDoc, challenge, id}) => {
+      return credentialServiceRPC.createPresentation({
+        credentials,
+        keyDoc,
+        challenge,
+        id,
+      });
+    },
+    [],
+  );
 
   return useMemo(() => ({presentCredentials}), [presentCredentials]);
 }
