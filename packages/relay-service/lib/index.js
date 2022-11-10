4a0574856d70be25d1c1b00f88105ce37e7718c5
Maycon Mello <maycon.mellos@gmail.com>
2022-11-10 10:58:42 -0300

    feat: relay service

--- packages/relay-service/lib/index.js
+++ packages/relay-service/lib/index.js
@@ -1,24 +1,24 @@
-import { getRealm } from '@docknetwork/wallet-sdk-core/lib/core/realm';
 import assert from 'assert';
-import { v4 as uuidv4 } from 'uuid';
+import axios from 'axios';
 
-// 'senderDid': senderDid,
-//   'recipientDid': recipientDid,
-//   'message': messageEncryptedWithRecipientKey
+let URL = 'https://relay.dock.io/';
 
-const sendMessage = async ({ senderDid, recipientDid, message }) => {
+const sendMessage = async ({senderDid, recipientDid, message}) => {
   assert(!!senderDid, 'senderDid is required');
   assert(!!recipientDid, 'recipientDid is required');
   assert(!!message, 'message is required');
+};
 
-}
-
-const getMessages = async ({ recipientDid }) => {
+const getMessages = async ({recipientDid}) => {
   assert(!!recipientDid, 'recipientDid is required');
+};
 
-}
+const setServiceURL = ({url}) => {
+  URL = url;
+};
 
 export const RelayService = {
   sendMessage,
   getMessages,
+  setServiceURL,
 };
