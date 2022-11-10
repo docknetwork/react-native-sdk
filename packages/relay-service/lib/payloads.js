7200bd2d336ac48f4890777a8b341bcc017f3482
Maycon Mello <maycon.mellos@gmail.com>
2022-11-10 11:05:01 -0300

    feat: relay service

--- packages/relay-service/lib/payloads.js
+++ packages/relay-service/lib/payloads.js
@@ -1,15 +1,9 @@
 import VerifiableCredential from '@docknetwork/sdk/verifiable-credential';
 import getKeyDoc from '@docknetwork/sdk/utils/vc/helpers';
 import {createNewDockDID} from '@docknetwork/sdk/utils/did';
-import {DIDKeyManager} from '@docknetwork/wallet-sdk-dids/src';
 import Keyring from '@polkadot/keyring';
 import {randomAsHex} from '@polkadot/util-crypto';
 
-// This API does not use the standard query/post parameters approach
-// instead, for security reasons, we create a credential for every read/write operation
-// we want to perform as a specific DID. The credential is then signed and compacted (see below method)
-// to keep the payload small. It is then converted to base64 for transport so that it can be used in headers, query or post body
-// in a live environment the below method would require to sign a credential for the subject by that DID (will be done with a client lib)
 export async function generatePayload(subject, _did) {
   const credentialId = 'http://example.edu/credentials/1986';
   const cred = new VerifiableCredential(credentialId);
