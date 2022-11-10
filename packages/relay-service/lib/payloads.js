c60cc94d55b1ffca433022245442a4ce53a82aac
Maycon Mello <maycon.mellos@gmail.com>
2022-11-10 11:04:13 -0300

    feat: relay service

--- packages/relay-service/lib/payloads.js
+++ packages/relay-service/lib/payloads.js
@@ -1,24 +1,30 @@
 import VerifiableCredential from '@docknetwork/sdk/verifiable-credential';
+import getKeyDoc from '@docknetwork/sdk/utils/vc/helpers';
+import {createNewDockDID} from '@docknetwork/sdk/utils/did';
 import {DIDKeyManager} from '@docknetwork/wallet-sdk-dids/src';
 import Keyring from '@polkadot/keyring';
-import {mnemonicGenerate} from '@polkadot/util-crypto';
+import {randomAsHex} from '@polkadot/util-crypto';
 
 // This API does not use the standard query/post parameters approach
 // instead, for security reasons, we create a credential for every read/write operation
 // we want to perform as a specific DID. The credential is then signed and compacted (see below method)
 // to keep the payload small. It is then converted to base64 for transport so that it can be used in headers, query or post body
 // in a live environment the below method would require to sign a credential for the subject by that DID (will be done with a client lib)
-export async function generatePayload(subject, did) {
+export async function generatePayload(subject, _did) {
   const credentialId = 'http://example.edu/credentials/1986';
   const cred = new VerifiableCredential(credentialId);
   cred.addSubject(subject);
 
   const keyring = new Keyring();
-  const phrase = mnemonicGenerate(12);
-  const pair = keyring.addFromMnemonic(phrase, null, 'ed25519');
-  const didDoc = await DIDKeyManager.keypairToDIDKeyDocument(pair);
+  const issuerDID = await createNewDockDID();
+  const issuerSeed = randomAsHex(32);
+  const issuerKey = getKeyDoc(
+    issuerDID,
+    keyring.addFromUri(issuerSeed, null, 'ed25519'),
+    'Ed25519VerificationKey2018',
+  );
 
-  await cred.sign(didDoc);
+  await cred.sign(issuerKey);
 
   return cred.toJSON();
 }
