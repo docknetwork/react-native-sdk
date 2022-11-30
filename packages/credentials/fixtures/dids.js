8482ffbecd16f80a2de003a46413a5a65c19d898
Maycon Mello <maycon.mellos@gmail.com>
2022-11-30 12:17:15 -0300

    fix: issues with web3-id auth

---
+++ packages/credentials/fixtures/dids.js
@@ -0,0 +1,26 @@
+export const didKeyDocument = {
+  '@context': ['https://w3id.org/wallet/v1'],
+  id: 'did:key:z6Mkstf77Jcodoii7CPmbUriutfrsfZgiLiqULNqNUjWazqw#z6Mkstf77Jcodoii7CPmbUriutfrsfZgiLiqULNqNUjWazqw',
+  controller: 'did:key:z6Mkstf77Jcodoii7CPmbUriutfrsfZgiLiqULNqNUjWazqw',
+  type: 'Ed25519VerificationKey2018',
+  publicKeyBase58: 'ESQ4X4NNJGEEzhZ4uutt4o7s46HqJTUUnKTuYCmVfn4Z',
+  privateKeyBase58:
+    'a3L6XcN7K4zFXnTfET1hLfPYpDSAoboEPuihVaYJoLCg1r5iAmpXHPKqAatoFELz7DoY86hQo9PBD8sjRMdLgnm',
+  publicKeyMultibase: 'zESQ4X4NNJGEEzhZ4uutt4o7s46HqJTUUnKTuYCmVfn4Z',
+  privateKeyMultibase:
+    'za3L6XcN7K4zFXnTfET1hLfPYpDSAoboEPuihVaYJoLCg1r5iAmpXHPKqAatoFELz7DoY86hQo9PBD8sjRMdLgnm',
+  correlation: ['636a9872-bf78-4294-b3ee-48e595d758d5'],
+};
+
+export const didDockDocument = {
+  id: 'did:dock:5G4q77h5UA82NY5MKHP1fJXg9NCN5JRgm1y8atRJhRrTPiJh#5F1VvjTArzSQEXDCmJey5p8RGtnqE13FRopXaSXwjoovmRmW',
+  controller: 'did:dock:5G4q77h5UA82NY5MKHP1fJXg9NCN5JRgm1y8atRJhRrTPiJh',
+  type: 'Sr25519VerificationKey2020',
+  publicKeyBase58: '9mV5onjLo16VivJwpG4hMYrN19iswcE2Mk2DZC4aNHhk',
+  privateKeyBase58:
+    '4NxmLWFMQveWapuHGxmjNdu6n1jNUuDh9Z6LdxWXZE4vZLcYDYiLHBqn5SmcgZZwvWjbM2SFfaA3pJHgzZQWN6My',
+  publicKeyMultibase: 'z9mV5onjLo16VivJwpG4hMYrN19iswcE2Mk2DZC4aNHhk',
+  privateKeyMultibase:
+    'z4NxmLWFMQveWapuHGxmjNdu6n1jNUuDh9Z6LdxWXZE4vZLcYDYiLHBqn5SmcgZZwvWjbM2SFfaA3pJHgzZQWN6My',
+  '@context': ['https://w3id.org/wallet/v1'],
+};
