c320ab1dee8b35cf5b086ba5abf11a626d3d8561
Maycon Mello <maycon.mellos@gmail.com>
2022-12-19 08:50:13 -0300

    feat: pex filters

--- packages/core/lib/services/credential/service-rpc.js
+++ packages/core/lib/services/credential/service-rpc.js
@@ -20,4 +20,10 @@ export class CredentialServiceRPC extends RpcService {
   async createBBSPresentation(params) {
     return this.call('createBBSPresentation', params);
   }
+  async filterCredentials(params) {
+    return this.call('filterCredentials', params);
+  }
+  async evaluatePresentation(params) {
+    return this.call('evaluatePresentation', params);
+  }
 }
