01a54baac4d38af73533b0609b9c7ceed7236823
Maycon Mello <maycon.mellos@gmail.com>
2023-03-06 11:32:12 -0300

    fix: issues with substrate

--- packages/core/lib/services/dock/service.js
+++ packages/core/lib/services/dock/service.js
@@ -62,7 +62,7 @@ export class DockService {
 
     Logger.info(`Attempt to initialized substrate at: ${params.address}`);
 
-    const result = await this.connectionInProgress;
+    await this.connectionInProgress;
 
     Logger.debug(`Substrate initialized at: ${params.address}`);
 
