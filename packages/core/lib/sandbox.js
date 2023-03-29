9f09432bb2571942307c94af0f1028a00d68f33a
Maycon Mello <maycon.mellos@gmail.com>
2023-03-28 21:38:36 -0300

    fix: fixing issues with json-rpc on sandbox

--- packages/core/lib/sandbox.js
+++ packages/core/lib/sandbox.js
@@ -2,66 +2,6 @@ import {getRpcClient, initRpcClient} from './rpc-client';
 import {setLogger} from './logger';
 import rpcServer from './sandbox-rpc-server';
 
-global.fetch = () => {
-  throw new Error('fetch is not available in the sandbox');
-};
-
-global.XMLHttpRequest = () => {
-  throw new Error('XMLHttpRequest is not available in the sandbox');
-};
-
-global.WebSocket = () => {
-  throw new Error('WebSocket is not available in the sandbox');
-};
-
-global.navigator = {
-  userAgent: 'React Native',
-  appVersion:
-    '5.0 (Linux; Android 6.0.1; Nexus 5 Build/M4B30Z) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.158 Mobile Safari/537.36',
-  platform: 'Linux armv8l',
-  product: 'ReactNative',
-  sendBeacon: () => {
-    throw new Error('sendBeacon is not available in the sandbox');
-  },
-};
-
-global.Worker = () => {
-  throw new Error('Worker is not available in the sandbox');
-};
-
-global.ServiceWorker = () => {
-  throw new Error('ServiceWorker is not available in the sandbox');
-};
-
-global.XMLHttpRequestEventTarget = () => {
-  throw new Error('XMLHttpRequestEventTarget is not available in the sandbox');
-};
-
-global.SharedWorker = () => {
-  throw new Error('SharedWorker is not available in the sandbox');
-};
-
-global.WebSocketEventTarget = () => {
-  throw new Error('WebSocketEventTarget is not available in the sandbox');
-};
-
-// Disable location and redirects
-global.location = {
-  href: '',
-  origin: '',
-  protocol: '',
-  host: '',
-  hostname: '',
-  port: '',
-  pathname: '',
-  search: '',
-  hash: '',
-  assign: () => {},
-  reload: () => {},
-  replace: () => {},
-  toString: () => '',
-};
-
 initRpcClient(jsonRPCRequest => {
   postMessage({
     type: 'json-rpc-request',
