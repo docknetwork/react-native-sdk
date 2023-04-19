63e32c35d5258b12843daca9c3129a035b263cf6
Maycon Mello <maycon.mellos@gmail.com>
2023-04-19 10:51:10 -0300

    feat: data store impl

---
+++ packages/data-store/src/types/types.ts
@@ -0,0 +1,33 @@
+export type Network = {
+  name: string;
+  id: string;
+};
+
+export type Wallet = {
+  name: string;
+  id: string;
+  network: string;
+  default?: boolean;
+};
+
+export type SDKConfigs = {
+  activeWallet: string;
+  version?: string;
+};
+
+export type DataStoreConfigs = {
+  wallets?: Wallet[];
+  networks?: Network[];
+  sdkConfigs?: SDKConfigs;
+
+  dropSchema?: boolean;
+};
+
+export type MigrationInput = {
+  sdkConfigs: SDKConfigs;
+};
+
+export type MigrationResult = {
+  migrated: boolean;
+  sdkConfigs: SDKConfigs;
+};
