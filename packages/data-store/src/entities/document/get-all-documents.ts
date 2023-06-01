4d61a6ce48fc66821b94835e2dd1b7cb9f379187
Maycon Mello <maycon.mellos@gmail.com>
2023-06-01 08:47:54 -0300

    fix: wallet backup for all networks, and removing wallet

--- packages/data-store/src/entities/document/get-all-documents.ts
+++ packages/data-store/src/entities/document/get-all-documents.ts
@@ -1,18 +1,39 @@
-import {WalletDocument} from '../../types';
+import {WalletDocument, DataStore} from '../../types';
 import {toWalletDocument} from './helpers';
 import {DocumentEntity} from './document.entity';
+
+function toWalletDocumentExpanded(entity: DocumentEntity): WalletDocument {
+  const result = toWalletDocument(entity);
+
+  if (result) {
+    result['_networkId'] = entity.networkId;
+  }
+
+  return result;
+}
 
 /**
  * Get all documents
  *
  * @param dataStore
  */
-export async function getAllDocuments({dataStore}): Promise<WalletDocument[]> {
+export async function getAllDocuments({
+  dataStore,
+  allNetworks,
+}: {
+  dataStore: DataStore;
+  allNetworks?: boolean;
+}): Promise<WalletDocument[]> {
   const repository = dataStore.db.getRepository(DocumentEntity);
   const entities = await repository.find({
-    where: {
-      networkId: dataStore.networkId,
-    },
+    where: allNetworks
+      ? {}
+      : {
+          networkId: dataStore.networkId,
+        },
   });
-  return Promise.all(entities.map(toWalletDocument));
+
+  const mapDocument = allNetworks ? toWalletDocumentExpanded : toWalletDocument;
+
+  return Promise.all(entities.map(mapDocument));
 }
