f7e300de35b58295a0bedb1f37142a17129553ba
Maycon Mello <maycon.mellos@gmail.com>
2023-07-07 08:27:55 -0300

    feat: refactoring

--- scripts/test-ci.sh
+++ scripts/test-ci.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# This script is used by the CI to run the tests in each package
 
 # Get the list of directories at the first level inside ./packages/
 directories=$(find ./packages/ -maxdepth 1 -type d)
@@ -7,3 +8,5 @@ directories=$(find ./packages/ -maxdepth 1 -type d)
 for dir in $directories; do
   yarn test --testMatch "<rootDir>/$dir/**/!(*.e2e).test.[jt]s"
 done
+
+yarn test --testMatch "<rootDir>/integration-tests/!(*.e2e).test.[jt]s"
