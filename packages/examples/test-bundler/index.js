e7e1bad8971f1592a21e68aab320872e04cc4aab
Maycon Mello <maycon.mellos@gmail.com>
2023-07-17 14:17:29 -0300

    feat: build modules

--- packages/examples/test-bundler/index.js
+++
@@ -1,7 +0,0 @@
-const {build} = require('@docknetwork/wallet-sdk-react-native/webpack');
-
-build({
-  entry: require.resolve('./bundler-test.js'),
-  path: `${__dirname}/build`,
-  filename: 'build.js',
-});
