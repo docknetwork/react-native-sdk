e4f1ca362ab9550d2f8f18641a65756eb87c4322
Maycon Mello <maycon.mellos@gmail.com>
2023-07-20 17:48:45 -0300

    fix: eslint configs

--- scripts/test-ci.sh
+++ scripts/test-ci.sh
@@ -6,6 +6,7 @@ directories=$(find ./packages/ -maxdepth 1 -type d)
 
 # Iterate over each directory and run the test command
 for dir in $directories; do
+  yarn build --cwd $dir
   yarn test --testMatch "<rootDir>/$dir/**/!(*.e2e).test.[jt]s"
 done
 
