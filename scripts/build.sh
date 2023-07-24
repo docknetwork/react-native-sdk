5ac3dc2df82c7b4ac018ff6075daa492cfe7e9d2
Maycon Mello <maycon.mellos@gmail.com>
2023-07-24 17:48:32 -0300

    fixing build issue with relay service

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,6 +4,4 @@ yarn workspace @docknetwork/wallet-sdk-data-store build
 yarn workspace @docknetwork/wallet-sdk-wasm build
 yarn workspace @docknetwork/wallet-sdk-core build
 yarn workspace @docknetwork/wallet-sdk-dids build
-
-
-
+yarn workspace @docknetwork/wallet-sdk-relay-service build
