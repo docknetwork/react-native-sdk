843bc18dea63c9e78c5f4b15aba28a96ce2012cf
Maycon Mello <maycon.mellos@gmail.com>
2023-08-01 16:28:37 -0300

    refactoring

--- packages/data-store/src/entities/document/helpers.ts
+++ packages/data-store/src/entities/document/helpers.ts
@@ -100,3 +100,9 @@ export async function toDocumentEntity({
     networkId: dataStore.networkId,
   } as DocumentEntity;
 }
+
+export const saveOptions = {
+  // Android is having issues when running multiple document saves in a short period of time
+  // We will disable transactions for now until we find a better solution
+  transaction: false,
+};
