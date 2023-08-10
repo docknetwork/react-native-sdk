3f9e757218d1c46612f591175edb2d8eb0cc52a5
Maycon Mello <maycon.mellos@gmail.com>
2023-08-10 06:23:38 -0300

    range proofs

---
+++ packages/wasm/src/services/dock/configs.ts
@@ -0,0 +1,12 @@
+// @ts-nocheck
+import assert from 'assert';
+
+export const validation = {
+  init(params) {
+    assert(!!params.address, `invalid substrate address ${params.address}`);
+  },
+};
+
+export type InitParams = {
+  address: string,
+};
