cad20c135e2e53db718ee3f119c881480ca3774c
Maycon Mello <maycon.mellos@gmail.com>
2024-01-24 10:47:33 -0300

    handle bbs+ accumulator witness

--- packages/core/src/credential-provider.ts
+++ packages/core/src/credential-provider.ts
@@ -3,6 +3,7 @@ import {IWallet} from './types';
 export interface ICredentialProvider {
   getCredentials(type?: string): any;
   isBBSPlusCredential(credential: any): boolean;
+  addCredential(credential: any): Promise<any>;
 }
 
 export function isBBSPlusCredential(credential) {
@@ -14,6 +15,26 @@ export function isBBSPlusCredential(credential) {
         context => context.bs && context.bs.indexOf('bbs') > -1,
       ))
   );
+}
+
+export async function addCredential({ wallet, credential }) {
+  const acummWitness = credential['$$accum_witness$$'];
+
+  if (acummWitness) {
+    delete credential['$$accum_witness$$'];
+  }
+
+  const response = await wallet.addDocument(credential);;
+
+  if (!acummWitness) {
+    await wallet.addDocument({
+      type: 'AccumulatorWitness',
+      id: `${credential.id}#witness`,
+      value: acummWitness,
+    })
+  }
+
+  return response;
 }
 
 export function createCredentialProvider({
@@ -28,6 +49,7 @@ export function createCredentialProvider({
   return {
     getCredentials,
     isBBSPlusCredential,
+    addCredential:(credential) => addCredential({ wallet, credential }),
     // TODO: move credential validity check to this provider
     // TODO: move import credential from json or URL to this provider
   };
