5c668cb4275d3a8f9baf8d61ca72a64e3bafe092
Maycon Mello <maycon.mellos@gmail.com>
2024-01-26 12:56:10 -0300

    updates for bbs+ revocation

---
+++ packages/core/src/credential-provider.test.ts
@@ -0,0 +1,34 @@
+import { ACUMM_WITNESS_PROP_KEY, ICredentialProvider, createCredentialProvider } from "./credential-provider";
+import { IWallet, createWallet } from "./wallet";
+
+import biometricsBBSRevocation from './fixtures/biometrics-credential-bbs-revocation.json';
+
+describe('CredentialProvider', () => {
+
+  let wallet: IWallet;
+  let provider: ICredentialProvider;
+
+  beforeEach(async () => {
+    wallet = await createWallet({
+      databasePath: ':memory:',
+    });
+    
+    provider = createCredentialProvider({ wallet });
+  });
+
+  describe('addCredential', () => {
+    it('should add a credential to the wallet', async () => {
+      await provider.addCredential({
+        ...biometricsBBSRevocation,
+      });
+
+      const cred = await provider.getById(biometricsBBSRevocation.id);
+
+      expect(cred.id).toBe(biometricsBBSRevocation.id);
+
+      const witness = await wallet.getDocumentById(`${biometricsBBSRevocation.id}#witness`);
+
+      expect(witness.value).toBe(biometricsBBSRevocation[ACUMM_WITNESS_PROP_KEY]);
+    });
+  });
+});
