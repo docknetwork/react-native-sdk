81f6cfa11998e99b71e60b9fd96c56469193983d
Maycon Mello <maycon.mellos@gmail.com>
2024-03-27 14:18:45 -0300

    refactoring on wallet to wallet verification flow

--- integration-tests/wallet-to-wallet-verification.test.ts
+++ integration-tests/wallet-to-wallet-verification.test.ts
@@ -8,21 +8,20 @@ import {createWalletToWalletVerificationProvider} from '@docknetwork/wallet-sdk-
 
 import {IDIDProvider} from '@docknetwork/wallet-sdk-core/src/did-provider';
 import {IMessageProvider} from '@docknetwork/wallet-sdk-core/src/message-provider';
+import {
+  Goals,
+  MessageTypes,
+} from '@docknetwork/wallet-sdk-core/src/messages/message-helpers';
 
-
-
-// function waitForMessages({ messageProvider }) {
-//   return new Promise((resolve) => {
-//     messageProvider.on
-//   });
-// }
-const waitFor = (ms) => new Promise((r) => setTimeout(r, ms));
+const waitFor = ms => new Promise(r => setTimeout(r, ms));
 
 describe('Wallet to Wallet Verification', () => {
   it('should get OOB message to be shared as QR code', async () => {
     const wallet: IWallet = await getWallet();
     const didProvider: IDIDProvider = getDIDProvider();
     const messageProvider: IMessageProvider = getMessageProvider();
+
+    await messageProvider.clearCache();
 
     const verificationProvider = createWalletToWalletVerificationProvider({
       wallet,
@@ -56,19 +55,93 @@ describe('Wallet to Wallet Verification', () => {
 
     // Verifier generates a QR Code with the invitation
     const invitation = await verificationProvider.getInvitationOOBMessage({
-      templateID: proofRequestTemplate.id,
+      templateId: proofRequestTemplate.id,
     });
 
     // Holder scans the QR Code and sends an ack
-    let messageHandlerResult = await verificationProvider.handleMessage(invitation);
+    let messageHandlerResult = await verificationProvider.handleMessage(
+      invitation,
+    );
 
     expect(messageHandlerResult).toBe(true);
 
-    messageProvider.startAutoFetch();
-    let message = await messageProvider.waitForMessage();
-    
+    const cancelAutoFetch = messageProvider.startAutoFetch();
 
-    console.log(message);
+    console.log('Verifier: Waiting for the ack message from the holder');
+    const ackMessage = await messageProvider.waitForMessage();
+    console.log(ackMessage);
+
+    expect(ackMessage.type).toBe(MessageTypes.Ack);
+    expect(ackMessage.body.goal_code).toBe(Goals.WalletToWalletVerification);
+
+    console.log('Verifier: ack message received');
+    console.log(
+      'Verifier: handling ack and sending proof request to the holder',
+    );
+    messageHandlerResult = await verificationProvider.handleMessage(ackMessage);
+
+    expect(messageHandlerResult).toBe(true);
+
+    console.log('Holder: Waiting for proof request from the verifier');
+    const proofRequestMessage = await messageProvider.waitForMessage();
+
+    console.log('Holder: Proof Request Message received');
+    console.log(proofRequestMessage);
+    expect(proofRequestMessage.type).toBe(MessageTypes.RequestPresentation);
+    expect(proofRequestMessage.body.proofRequestId).toEqual(
+      proofRequestTemplate.id,
+    );
+    expect(proofRequestMessage.body.proofRequest).toEqual(templateJSON);
+
+
+    verificationProvider.setProofRequestHandler((_proofRequest) => {
+      // could render a UI to ask the user to provide the requested data
+      console.log('Holder: Generating presentation');
+      const presentation = {
+        id: 'presentationId',
+        type: 'Presentation',
+        verifiableCredential: [
+          {
+            id: 'Credential 1',
+            name: 'Any credential',
+            purpose: 'Any credential',
+          },
+        ],
+      };
+      return presentation;
+    });
+
+    verificationProvider.handleMessage(proofRequestMessage);
+
+    console.log('Verifier: Waiting for the presentation from the holder');
+    const presentationMessage = await messageProvider.waitForMessage();
+
+    expect(presentationMessage.type).toBe(MessageTypes.Presentation);
+    console.log('Verifier: Presentation received');
+
+    verificationProvider.setPresentationHandler((_presentation) => {
+      // could render a UI to ask the user to verify the presentation
+      console.log('Verifier: Verifying the presentation');
+      return {
+        verifier: true,
+        message: 'Presentation verified',
+      };
+    });
+
+    verificationProvider.handleMessage(presentationMessage);
+
+    console.log('Holder: Waiting for the ack from the verifier');
+    const ackPresentationMessage = await messageProvider.waitForMessage();
+
+    expect(ackPresentationMessage.type).toBe(MessageTypes.Ack);
+    expect(ackPresentationMessage.body.goal_code).toBe(
+      Goals.PresentationAckFromVerifier,
+    );
+    console.log('Holder: Ack received');
+
+    console.log(ackPresentationMessage);
+
+    cancelAutoFetch();
 
     await waitFor(3000);
   });
