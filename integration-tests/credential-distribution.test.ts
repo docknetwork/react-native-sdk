a30904d46e038054178061f97411fa0e2df07ccb
Maycon Mello <maycon.mellos@gmail.com>
2024-03-29 15:36:10 -0300

    fixing issues with bbs+ required fields

--- integration-tests/credential-distribution.test.ts
+++ integration-tests/credential-distribution.test.ts
@@ -38,30 +38,30 @@ function issueCredential({subjectDID}) {
   );
 }
 describe('Credential Distribution', () => {
-  // it('should receive a credential using did distribution', async () => {
-  //   const wallet = await getWallet();
-  //   const currentDID = await getDIDProvider().getDefaultDID();
+  it('should receive a credential using did distribution', async () => {
+    const wallet = await getWallet();
+    const currentDID = await getDIDProvider().getDefaultDID();
 
-  //   let time = new Date().getTime();
-  //   console.log('Issue credential using certs');
-  //   const result = await issueCredential({
-  //     subjectDID: currentDID,
-  //   });
+    let time = new Date().getTime();
+    console.log('Issue credential using certs');
+    const result = await issueCredential({
+      subjectDID: currentDID,
+    });
 
-  //   console.log(`Credential issued in ${new Date().getTime() - time} ms`);
+    console.log(`Credential issued in ${new Date().getTime() - time} ms`);
 
-  //   time = new Date().getTime();
-  //   console.log('Waiting for distribution message....');
+    time = new Date().getTime();
+    console.log('Waiting for distribution message....');
 
-  //   getMessageProvider().startAutoFetch();
+    getMessageProvider().startAutoFetch();
 
-  //   const message = await getMessageProvider().waitForMessage();
+    const message = await getMessageProvider().waitForMessage();
 
-  //   console.log(`Credential received in ${new Date().getTime() - time} ms`);
-  //   expect(message.type).toBe(
-  //     'https://didcomm.org/issue-credential/2.0/issue-credential',
-  //   );
-  // });
+    console.log(`Credential received in ${new Date().getTime() - time} ms`);
+    expect(message.type).toBe(
+      'https://didcomm.org/issue-credential/2.0/issue-credential',
+    );
+  });
 
   it('should receive multiple credentials using multiple DIDS', async () => {
     const wallet = await getWallet();
