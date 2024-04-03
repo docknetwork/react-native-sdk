ba05237ac0fdb9a1a20230efbcbdeac0d062083a
Maycon Mello <maycon.mellos@gmail.com>
2024-04-03 15:23:44 -0300

    remove optional attribute from pex filter

--- packages/wasm/src/services/pex/service.ts
+++ packages/wasm/src/services/pex/service.ts
@@ -13,10 +13,7 @@ const pex: PEX = new PEX();
 function removeOptionalAttribute(presentationDefinition) {
   presentationDefinition.input_descriptors.forEach(inputDescriptor => {
     if (inputDescriptor.constraints && inputDescriptor.constraints.fields) {
-      inputDescriptor.constraints.fields = inputDescriptor.constraints.fields.map(field => {
-        const { optional, ...remainingAttributes } = field;
-        return remainingAttributes;
-      });
+      inputDescriptor.constraints.fields = inputDescriptor.constraints.fields.filter(field => field.optional !== true);
     }
   });
 
