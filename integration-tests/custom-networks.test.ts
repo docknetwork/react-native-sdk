eee54651e9de9bedc88a4c3a5047483e51207735
Maycon Mello <maycon.mellos@gmail.com>
2024-04-09 14:29:08 -0300

    DCKM-432: Fixing issues with custom network tests

--- integration-tests/custom-networks.test.ts
+++ integration-tests/custom-networks.test.ts
@@ -1,5 +1,6 @@
 import {createWallet, IWallet} from '@docknetwork/wallet-sdk-core/src/wallet';
 import {Network} from '@docknetwork/wallet-sdk-data-store/src/types';
+import {WalletEvents} from '@docknetwork/wallet-sdk-wasm/src/modules/wallet';
 
 describe('Custom networks', () => {
   let wallet: IWallet;
@@ -8,22 +9,30 @@ describe('Custom networks', () => {
       name: 'polygon',
       credentialHostnames: ['creds.polygon.com'],
       id: 'polygon',
-      configs: '{}',
+      configs: {
+        substrateUrl: 'wss://knox-1.dock.io',
+        addressPrefix: 21,
+      },
     },
     {
       name: 'Polygon mumbai',
       credentialHostnames: ['creds.mumbai.polygon.com'],
       id: 'mumbai',
-      configs: '{}',
+      configs: {
+        substrateUrl: 'wss://knox-1.dock.io',
+        addressPrefix: 21,
+      },
     },
   ];
 
   const mockDocuments = {
     mainnet: {
+      id: 'mainnet-doc',
       value: 'mainnet doc',
       type: 'JWT-VC',
     },
     testnet: {
+      id: 'testnet-doc',
       value: 'testnet doc',
       type: 'JWT-VC',
     },
@@ -34,39 +43,43 @@ describe('Custom networks', () => {
       databasePath: ':memory:',
       networks,
     });
+
+    await wallet.waitForEvent(WalletEvents.networkConnected);
+
+    console.log('adding documents to the wallet');
     await wallet.addDocument(mockDocuments.mainnet);
   });
 
   it('should have documents only on mainnet', async () => {
-    const [doc] = await wallet.getAllDocuments();
+    const doc = await wallet.getDocumentById(mockDocuments.mainnet.id);
+
     expect(doc.value).toEqual(mockDocuments.mainnet.value);
 
     wallet.setNetwork('mumbai');
 
-    let mumbaiDocs = await wallet.getAllDocuments();
+    let mumbaiDocc = await wallet.getDocumentById(mockDocuments.testnet.id);
 
-    expect(mumbaiDocs).toHaveLength(0);
+    expect(mumbaiDocc).toBeNull();
   });
 
   it('should add document to mumbai without affecting polygon', async () => {
     wallet.setNetwork('mumbai');
 
     await wallet.addDocument(mockDocuments.testnet);
-    let [doc] = await wallet.getAllDocuments();
+    let doc = await wallet.getDocumentById(mockDocuments.testnet.id);
     expect(doc.value).toEqual(mockDocuments.testnet.value);
 
     wallet.setNetwork('polygon');
-    const allDocs = await wallet.getAllDocuments();
-    [doc] = allDocs;
-    expect(allDocs).toHaveLength(1);
+    doc = await wallet.getDocumentById(mockDocuments.mainnet.id);
     expect(doc.value).toEqual(mockDocuments.mainnet.value);
   });
 
   afterAll(async () => {
-    return new Promise((res) => {
+    return new Promise(res => {
       setTimeout(() => {
-        res(wallet.dataStore.db.destroy());
-      }, 400);
+        wallet.dataStore.db.destroy();
+        res(null);
+      }, 2000);
     });
   });
 });
