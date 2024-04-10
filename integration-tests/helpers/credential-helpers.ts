cc340bee841d8ff18b2091099fec50f461902de7
Maycon Mello <maycon.mellos@gmail.com>
2024-04-10 12:49:12 -0300

    DCKM-432: Fixing issues with integration tests

--- integration-tests/helpers/credential-helpers.ts
+++ integration-tests/helpers/credential-helpers.ts
@@ -2,7 +2,7 @@ import {Credentials} from '@docknetwork/wallet-sdk-credentials/lib';
 import {getWallet} from './wallet-helpers';
 
 export async function getCredentials() {
-  Credentials.getInstance().wallet = getWallet();
+  Credentials.getInstance().wallet = await getWallet();
   return Credentials.getInstance().query({});
 }
 
@@ -12,11 +12,11 @@ export async function getCredentialById(id) {
 }
 
 export async function importCredentialJSON(json) {
-  Credentials.getInstance().wallet = getWallet();
+  Credentials.getInstance().wallet = await getWallet();
   return Credentials.getInstance().add(json);
 }
 
 export async function removeCredential(credentialId) {
-  Credentials.getInstance().wallet = getWallet();
+  Credentials.getInstance().wallet = await getWallet();
   return Credentials.getInstance().remove(credentialId);
 }
