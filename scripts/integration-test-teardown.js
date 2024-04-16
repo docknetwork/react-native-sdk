e4e856d11b93520767691e56e8d41ec96f6e4fb9
Maycon Mello <maycon.mellos@gmail.com>
2024-04-16 12:16:05 -0300

    fixing open handle issues

--- scripts/integration-test-teardown.js
+++ scripts/integration-test-teardown.js
@@ -1,1 +1,12 @@
-module.exports = () => {};
+import dock from '@docknetwork/sdk';
+
+module.exports = async () => {
+  if (dock.isConnected) {
+    try {
+      console.log('Disconnecting from Dock node');
+      await dock.disconnect();
+    } catch (err) {
+      console.error('Error disconnecting from Dock node', err);
+    }
+  }
+};
