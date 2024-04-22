5612731ab72164160696988415f38ce070af3d95
Maycon Mello <maycon.mellos@gmail.com>
2024-04-22 15:55:25 -0300

    DCKM-461: fixing issues with pex for optional attributes

--- packages/wasm/src/services/pex/service.ts
+++ packages/wasm/src/services/pex/service.ts
@@ -17,40 +17,40 @@ const pex: PEX = new PEX();
  * This is a temporary workaround until the issue is fixed in the @sphereon/pex library
  **/
 export function removeOptionalAttribute(presentationDefinition) {
-  // presentationDefinition.input_descriptors.forEach(inputDescriptor => {
-  //   if (!inputDescriptor.constraints?.fields?.length) {
-  //     return;
-  //   }
-  //   // Filter the optional fields
-  //   // If we include those fields, it might exclude few credentials from the resulsts
-  //   // e.g: Expiration date as optional, a credenntial without expiration date should be included
-  //   inputDescriptor.constraints.fields =
-  //     inputDescriptor.constraints.fields.filter(
-  //       field => field.optional !== true,
-  //     );
+  presentationDefinition.input_descriptors.forEach(inputDescriptor => {
+    if (!inputDescriptor.constraints?.fields?.length) {
+      return;
+    }
+    // Filter the optional fields
+    // If we include those fields, it might exclude few credentials from the resulsts
+    // e.g: Expiration date as optional, a credenntial without expiration date should be included
+    inputDescriptor.constraints.fields =
+      inputDescriptor.constraints.fields.filter(
+        field => field.optional !== true,
+      );
 
-  //   // Removes the optinal attributes from the fields
-  //   // It applies in case optional: false
-  //   // The field is required, but pex doesn't support the attribute
-  //   inputDescriptor.constraints.fields.forEach(field => {
-  //     if (field.optional !== undefined) {
-  //       delete field.optional;
-  //     }
-  //   });
+    // Removes the optinal attributes from the fields
+    // It applies in case optional: false
+    // The field is required, but pex doesn't support the attribute
+    inputDescriptor.constraints.fields.forEach(field => {
+      if (field.optional !== undefined) {
+        delete field.optional;
+      }
+    });
 
-  //   // There is a case where ALL fields are optional
-  //   // If we remove all fields, it will cause an error with PEX
-  //   // So, we add a placeholder field to avoid the error
-  //   if (inputDescriptor.constraints.fields.length === 0) {
-  //     inputDescriptor.constraints.fields.push({
-  //       path: '$.id',
-  //     });
-  //   }
-  // });
+    // There is a case where ALL fields are optional
+    // If we remove all fields, it will cause an error with PEX
+    // So, we add a placeholder field to avoid the error
+    if (inputDescriptor.constraints.fields.length === 0) {
+      inputDescriptor.constraints.fields.push({
+        path: '$.id',
+      });
+    }
+  });
 
-  // if (!presentationDefinition.id) {
-  //   presentationDefinition.id = 'placeholder';
-  // }
+  if (!presentationDefinition.id) {
+    presentationDefinition.id = 'id';
+  }
 
   return presentationDefinition;
 }
