c2da9fac6ab313cd01f265c116a7a97489e345d7
Johnson Tinuoye <johnsontinuoye@gmail.com>
2024-05-07 10:08:10 +0100

    fix: test documents hooks

--- packages/react-native/lib/index.tsx
+++ packages/react-native/lib/index.tsx
@@ -53,9 +53,12 @@ export const WalletSDKContext = React.createContext<WalletSDKContextProps>({
 });
 
 setStorage(AsyncStorage);
+
 export {useAccounts};
 export {useDIDManagement};
 export {useCredentialUtils, useCredentialStatus};
+export {useDocument, useDocuments} from './documentsHooks';
+
 export function getStorage() {
   return AsyncStorage;
 }
@@ -103,76 +106,6 @@ export function useAccount(address) {
     onDelete,
   };
 }
-
-const useEventListener = (eventManager, eventNames, listener) => {
-  useEffect(() => {
-    eventNames.forEach(eventName => eventManager.on(eventName, listener));
-    return () =>
-      eventNames.forEach(eventName =>
-        eventManager.removeListener(eventName, listener),
-      );
-  }, [eventManager, eventNames, listener]);
-};
-
-const events = [
-  WalletEvents.documentAdded,
-  WalletEvents.documentRemoved,
-  WalletEvents.documentUpdated,
-];
-
-export function useDocument(id) {
-  const [document, setDocument] = useState(null);
-
-  const refetchDocument = useCallback(
-    async updatedDoc => {
-      if (updatedDoc.id !== id) return;
-      const doc = await getWallet().getDocumentById(id);
-      setDocument(doc);
-    },
-    [id],
-  );
-
-  useEffect(() => {
-    getWallet().getDocumentById(id).then(setDocument);
-  }, [id]);
-
-  useEventListener(getWallet().eventManager, events, refetchDocument);
-
-  return document;
-}
-
-export function useDocuments({type}) {
-  const [documents, setDocuments] = useState([]);
-  const [loading, setLoading] = useState(true);
-
-  const fetchDocuments = useCallback(
-    async (updatedDoc, forceFetch = false) => {
-      if (
-        forceFetch ||
-        updatedDoc?.type === type ||
-        updatedDoc?.type?.includes(type)
-      ) {
-        const docs = await getWallet().getDocumentsByType(type);
-        setDocuments(docs);
-        setLoading(false);
-      }
-    },
-    [type],
-  );
-
-  useEffect(() => {
-    fetchDocuments(null, true);
-  }, [fetchDocuments, setLoading]);
-
-  useEventListener(getWallet().eventManager, events, fetchDocuments);
-  useEventListener(getWallet().eventManager, [WalletEvents.networkUpdated], ()=> fetchDocuments(null, true));
-
-  return {
-    documents,
-    loading,
-  };
-}
-
 export function useWallet() {
   return useContext(WalletSDKContext);
 }
