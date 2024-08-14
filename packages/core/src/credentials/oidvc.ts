2f0ad1628de49459e0eb4e590a0bf44b1cb7ba6e
Maycon <maycon.mellos@gmail.com>
2024-08-14 15:44:09 -0300

    DCKM-579: support for OID4VC import

---
+++ packages/core/src/credentials/oidvc.ts
@@ -0,0 +1,71 @@
+import {OpenID4VCIClientV1_0_13} from '@sphereon/oid4vci-client';
+import {IWallet} from '../types';
+import {IDIDProvider} from '../did-provider';
+import {Alg} from '@sphereon/oid4vci-common';
+import {didServiceRPC} from '@docknetwork/wallet-sdk-wasm/src/services/dids';
+
+export async function getOpenIDCredentialFromURI({
+  didProvider,
+  uri,
+  getAuthCode,
+}: {
+  didProvider: IDIDProvider;
+  uri: string;
+  getAuthCode?: (authorizationURL: string) => Promise<string>;
+}) {
+  const [holderKeyDocument] = await didProvider.getDIDKeyPairs();
+
+  const queryString = uri.split('?')[1];
+  const params = new URLSearchParams(queryString);
+  const credentialOfferEncoded = params.get('credential_offer');
+  const credentialOfferDecoded = decodeURIComponent(credentialOfferEncoded);
+  const credentialOffer = JSON.parse(credentialOfferDecoded);
+  const scope = credentialOffer.credentials[0];
+  const format = 'ldp_vc';
+  const credentialTypes = scope.replace('ldp_vc:', '');
+
+  const client = await OpenID4VCIClientV1_0_13.fromURI({
+    uri: uri,
+    clientId: 'dock.wallet',
+    authorizationRequest: {
+      redirectUri: 'dock-wallet://credentials/callback',
+      clientId: 'dock.wallet',
+      scope: credentialOffer.credentials[0],
+    },
+  });
+
+  let code;
+
+  if (client.credentialOffer?.preAuthorizedCode) {
+    code = client.credentialOffer?.preAuthorizedCode;
+  } else {
+    code = await getAuthCode(client.authorizationURL); 
+    // TODO: Remove this return
+    return;
+  }
+
+  await client.acquireAccessToken({
+    code,
+  });
+
+  const credentialResponse = await client.acquireCredentials({
+    credentialTypes,
+    proofCallbacks: {
+      signCallback: async args => {
+        // use service method here
+        const jwt = await didServiceRPC.createSignedJWT({
+          payload: args.payload,
+          privateKeyDoc: holderKeyDocument,
+          headerInput: args.header,
+        });
+
+        return jwt;
+      },
+    },
+    format: format,
+    alg: Alg.EdDSA,
+    kid: holderKeyDocument.id,
+  });
+
+  return credentialResponse;
+}
--- packages/wasm/src/services/dids/service.ts
+++ packages/wasm/src/services/dids/service.ts
@@ -9,11 +9,43 @@ import {keyringService} from '../keyring/service';
 import {utilCryptoService} from '../util-crypto/service';
 import assert from 'assert';
 import {createNewDockDID} from '@docknetwork/sdk/utils/did';
-import {getDock} from '../dock/service';
+import {dockService, getDock} from '../dock/service';
 import {PublicKeySr25519} from '@docknetwork/sdk';
 import {DidKey, VerificationRelationship} from '@docknetwork/sdk/public-keys';
 import {Logger} from '../../core/logger';
 import {polkadotToKeydoc} from '../../core/polkadot-utils';
+import base64url from 'base64url';
+import { keyDocToKeypair } from '../credential/utils';
+import Ed25519Signature2020 from '@docknetwork/sdk/utils/vc/crypto/Ed25519Signature2020';
+import EcdsaSecp256k1Signature2019 from '@docknetwork/sdk/utils/vc/crypto/EcdsaSecp256k1Signature2019';
+
+async function getSignerKeypair(privateKeyDoc) {
+  const privateKey =
+    privateKeyDoc.keypair || keyDocToKeypair(privateKeyDoc, dockService.dock);
+
+  if (!privateKey.signer && privateKey.sign) {
+    privateKey.signer = () => ({sign: ({data}) => privateKey.sign(data)});
+  }
+
+  // ed25519 should use latest
+  if (privateKeyDoc.type.startsWith('Ed25519')) {
+    return new Ed25519Signature2020({
+      keypair: privateKey,
+      ...privateKeyDoc,
+    });
+  }
+
+  // HACK: for secp256k1 keyDocToKeypair returns an unusable class, wrap it in EcdsaSecp256k1Signature2019
+  // TODO: refactor so that keyDocToKeypair returns EcdsaSecp256k1Signature2019
+  if (privateKey.type === 'secp256k1') {
+    return new EcdsaSecp256k1Signature2019({
+      keypair: privateKey,
+      ...privateKeyDoc,
+    });
+  }
+
+  return privateKey;
+}
 
 class DIDService {
   name: string;
@@ -27,6 +59,8 @@ class DIDService {
     DIDService.prototype.generateKeyDoc,
     DIDService.prototype.registerDidDock,
     DIDService.prototype.getDidDockDocument,
+    DIDService.prototype.createSignedJWT,
+
   ];
   keypairToDIDKeyDocument(params: KeypairToDIDKeyDocumentParams) {
     validation.keypairToDIDKeyDocument(params);
@@ -58,6 +92,30 @@ class DIDService {
     }
 
     return polkadotToKeydoc(keyring, params.controller);
+  }
+
+  async createSignedJWT({
+    payload,
+    privateKeyDoc,
+    headerInput
+  }) {
+    const privateKey = await getSignerKeypair(privateKeyDoc);
+    const {sign} =
+      typeof privateKey.signer === 'function'
+        ? privateKey.signer()
+        : privateKey.signer;
+    const header = {
+      alg: privateKey.alg || 'EdDSA',
+      kid: privateKeyDoc.id,
+      ...headerInput,
+    };
+    const headerBase64URL = base64url(JSON.stringify(header));
+    const payloadBase64URL = base64url(JSON.stringify(payload));
+    const headerAndPayloadBase64URL = `${headerBase64URL}.${payloadBase64URL}`;
+    const signPayload = Buffer.from(headerAndPayloadBase64URL);
+  
+    const signature = await sign({data: signPayload});
+    return `${headerAndPayloadBase64URL}.${base64url.encode(signature)}`;
   }
 
   async getDidDockDocument(did) {
