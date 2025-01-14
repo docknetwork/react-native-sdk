7b2b7b10aefd7cee6d30b2c071cb4edef3bdea71
Maycon <maycon.mellos@gmail.com>
2025-01-14 14:25:38 -0300

    removeing old crypto-wasm-ts reference

--- scripts/fix-deps.sh
+++ scripts/fix-deps.sh
@@ -1,4 +1,4 @@
 rm -rf packages/dids/node_modules/@digitalbazaar
 
-# TODO: Remove @docknetwork/sdk dependency from wallet-sdk/cli
+# TODO: Replace @docknetwork/sdk dependency from wallet-sdk/cli
 rm -rf ./node_modules/@docknetwork/sdk/node_modules/@docknetwork/crypto-wasm-ts
