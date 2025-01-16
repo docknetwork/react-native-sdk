8ef42673611da459fe431010181e7ce1843a4be5
Maycon <maycon.mellos@gmail.com>
2025-01-16 16:59:37 -0300

    update the max_number placeholder for range proofs

--- packages/wasm/src/services/credential/pex-helpers.js
+++ packages/wasm/src/services/credential/pex-helpers.js
@@ -1,11 +1,11 @@
 import {JSONPath} from '@astronautlabs/jsonpath';
 
-export const EPSILON_NUMBER = 0.0001;
+export const EPSILON_NUMBER = 0.001;
 export const EPSILON_INT = 1;
 
 export const MAX_DATE_PLACEHOLDER = 884541351600000;
 export const MIN_DATE_PLACEHOLDER = -17592186044415;
-export const MAX_NUMBER = 100 ** 6;
+export const MAX_NUMBER = 100 ** 5;
 export const MIN_NUMBER = -4294967294;
 
 /*
