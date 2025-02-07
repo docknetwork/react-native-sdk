339f3b68701076f11bc6127ce023ae58607752a8
Maycon <maycon.mellos@gmail.com>
2025-02-07 14:00:12 -0300

    padding blockNo when updating accumulator witness

--- packages/wasm/src/services/credential/bbs-revocation.ts
+++ packages/wasm/src/services/credential/bbs-revocation.ts
@@ -64,7 +64,7 @@ export const getWitnessDetails = async (credential, _membershipWitness) => {
       registryId,
       encodedRevId,
       membershipWitness,
-      queriedAccumulator.created,
+      blockNo,
       queriedAccumulator.lastModified,
     );
   } catch (err) {
