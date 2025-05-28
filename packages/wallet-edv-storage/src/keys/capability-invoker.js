e8be215a4611ac6e7714bf96b88b263f9b9f4d24
Sam Hellawell <sshellawell@gmail.com>
2025-05-28 19:58:43 +0100

    fix lint

--- packages/wallet-edv-storage/src/keys/capability-invoker.js
+++ packages/wallet-edv-storage/src/keys/capability-invoker.js
@@ -4,13 +4,16 @@ export default class CapabilityInvoker {
     this.id = key.id;
   }
 
-  async sign({ data }) {
+  async sign({data}) {
     const signer = this.key.signer();
-    return signer.sign({ data });
+    return signer.sign({data});
   }
 
   async toJson(exportPrivateKey = false) {
-    const keypair = await this.key.export({ type: 'JsonWebKey2020', privateKey: exportPrivateKey });
+    const keypair = await this.key.export({
+      type: 'JsonWebKey2020',
+      privateKey: exportPrivateKey,
+    });
     if (keypair.id.indexOf('#') === 0) {
       keypair.id = keypair.controller + keypair.id;
     }
